7b31f92bdd1da42dd14e9d17486aa6b77a906eb1
Tim <timmymclennan@gmail.com>
2024-01-12 17:16:58 +1100

    Setup Redux and Initialised server folder.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@/index.css'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { configureStore } from '@reduxjs/toolkit'
+import { api } from './state/api.ts'
+import { Provider } from 'react-redux'
+
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+})
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
