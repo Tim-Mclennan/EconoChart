2ee86371a51b5aaf9f7b84f79a444f194281786c
Tim <timmymclennan@gmail.com>
2024-01-16 18:15:19 +1100

    integrated Lineargradient components to render data

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -32,15 +32,39 @@ const Row1 = () => {
                         height={400}
                         data={revenueExpenses}
                         margin={{
-                            top: 10,
-                            right: 30,
-                            left: 0,
-                            bottom: 0,
+                            top: 15,
+                            right: 25,
+                            left: -10,
+                            bottom: 60,
                         }}
                     >
-                        <CartesianGrid strokeDasharray="3 3" />
+                        <defs>
+                            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                                <stop
+                                    offset="5%"
+                                    stopColor={palette.primary[300]}
+                                    stopOpacity={0.5}
+                                />
+                                <stop
+                                    offset="95%"
+                                    stopColor={palette.primary[300]}
+                                    stopOpacity={0}
+                                />
+                            </linearGradient>
+                            <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+                                <stop
+                                    offset="5%"
+                                    stopColor={palette.primary[300]}
+                                    stopOpacity={0.5}
+                                />
+                                <stop
+                                    offset="95%"
+                                    stopColor={palette.primary[300]}
+                                    stopOpacity={0}
+                                />
+                            </linearGradient>
+                        </defs>
                         <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }} />
-                        
                         <YAxis
                             dataKey="name" domain={[8000, 23000]} axisLine={{ strokeWidth: "0" }} style={{ fontSize: "10px" }}
                         />
