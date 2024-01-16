88e4512629c5725b71ed27f0611e73d9fbadbefd
Tim <timmymclennan@gmail.com>
2024-01-16 14:10:44 +1100

    Refactored schema for KPIs

--- server/models/KPI.js
+++ server/models/KPI.js
@@ -5,90 +5,80 @@ const Schema = mongoose.Schema;
 loadType(mongoose);
 
 const daySchema = new Schema(
-    {
-        date: String,
-        revenue: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        }, 
-        expenses: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        },
+  {
+    date: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
     },
-    { toJson: { getters: true } }
-)
-
-const monthSchema = new Schema(
-    {
-        month: String,
-        revenue: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        }, 
-        expenses: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        },
-        operationalExpenses: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        },
-        nonOperationalExpenses: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
     },
-    { toJson: { getters: true } }
-)
-
-// Define a schema for the category expense
-const CategoryExpenseSchema = new Schema({
-  category: String,
-  amount: {
-    type: Number,
-    get: v => (v / 100).toFixed(2) // Assuming you want to store currency in cents and convert to dollars
-  }
-});
-
-// Create a model for the category expense
-const CategoryExpense = mongoose.model('CategoryExpense', CategoryExpenseSchema);
-
-
-const KPISchema = new Schema(
-    {
-        totalProfit: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        },
-        totalRevenue: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        },
-        totalExpenses: {
-            type: mongoose.Types.Currency,
-            currency: "AUD",
-            get: (v) => v / 100
-        },
-        expenseByCategory: [{
-            type: Schema.Types.ObjectId,
-            ref: CategoryExpense
-          }],
-        monthlyData: [monthSchema],
-        dailyData: [daySchema],
-
-    },
-    { timestamps: true, toJSON: { getters: true }}
+  },
+  { toJSON: { getters: true } }
 );
 
-const KPI = mongoose.model("KPI", KPISchema)
+const monthSchema = new Schema(
+  {
+    month: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
+    },
+    operationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
+    },
+    nonOperationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
+    },
+  },
+  { toJSON: { getters: true } }
+);
+
+const KPISchema = new Schema(
+  {
+    totalProfit: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
+    },
+    totalRevenue: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
+    },
+    totalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "AUD",
+      get: (v) => v / 100,
+    },
+    expensesByCategory: {
+      type: Map,
+      of: {
+        type: mongoose.Types.Currency,
+        currency: "AUD",
+        get: (v) => v / 100,
+      },
+    },
+    monthlyData: [monthSchema],
+    dailyData: [daySchema],
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
+
+const KPI = mongoose.model("KPI", KPISchema);
 
 export default KPI;
